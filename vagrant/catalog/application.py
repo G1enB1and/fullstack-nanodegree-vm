c61a8443d9565fceda3f25a146b0823e9307a2df
G1enB1and <glen.bland.81@gmail.com>
2018-09-12 09:50:40 -0500

    show items in category by category number urls

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from database_setup import Base, Catalog, CatalogItem
+from database_setup import Base, Category, CatalogItem
 app = Flask(__name__)
 
 
@@ -15,13 +15,26 @@ session = DBSession()
 @app.route('/')
 @app.route('/catalog')
 def HelloWorld():
-    category = session.query(Category).filter_by(id = 3).one()
+    category = session.query(Category).filter_by(id = 1).one()
     items = session.query(CatalogItem).filter_by(category_id=category.id)
     output = ''
     for i in items:
         output += i.name
         output += '</br>'
     return output
+
+
+@app.route('/category/<int:category_id>/')
+@app.route('/category/<int:category_id>/items')
+def ShowItemsInCategory(category_id):
+    category = session.query(Category).filter_by(id = category_id).one()
+    items = session.query(CatalogItem).filter_by(category_id=category.id)
+    output = ''
+    for i in items:
+        output += i.name
+        output += '</br>'
+    return output
+
 
 if __name__ == '__main__':
     app.debug = True
