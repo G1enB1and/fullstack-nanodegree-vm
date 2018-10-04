244300de7263acd104af61ae3ad2c3eb775ba3d4
G1enB1and <glen.bland.81@gmail.com>
2018-10-04 11:10:03 -0500

    added user to database
    added user table to database_setup and user1 for all items in populate_catalog

--- vagrant/catalog/populate_catalog.py
+++ vagrant/catalog/populate_catalog.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from database_setup import Category, Base, CatalogItem
+from database_setup import Category, Base, CatalogItem, User
 
 engine = create_engine('sqlite:///catalog.db')
 # Bind the engine to the metadata of the Base class so that the
@@ -19,132 +19,188 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 
+# Create dummy user
+User1 = User(name="Totally Legit Robot", email="legit.name@totally_legit_store.com", picture="robot.jpg")
+
+session.add(User1)
+session.commit()
+
+
 # Catelog Items for Category 1 (Electronics)
-category1 = Category(name="Electronics")
+category1 = Category(user_id=1, name="Electronics")
 
 session.add(category1)
 session.commit()
 
 
-catalogItem1 = CatalogItem(name="Bluetooth Headset", description="wireless headset for calls and music. Blootooth 4.2, range of 30 ft.",
-                           price="$49.99", category=category1)
+catalogItem1 = CatalogItem(user_id=1, name="Wireless Headset", description="This wireless headset features two miniature faeries that sing, talk, and beat-box with magically enhanced accuracy inside the ear buds. Just don't ask them to play comedy. Sometimes they can't help but laugh. Have you ever heard someone laugh from inside your ear? ouch.",
+                           price="$59.99", category=category1)
 
 session.add(catalogItem1)
 session.commit()
 
-catalogItem2 = CatalogItem(name="65 inch 4K Smart TV", description="WebOS 4.0 with built in Google Assistant, Magic Remote, and SmartThinQ",
-                           price="$1,999", category=category1)
+catalogItem2 = CatalogItem(user_id=1, name="Big Screen TV", description="This portal into other realms is protected by a clear plastic shield that won't let you pass into it. Just don't press too hard. This particular model measures 65\".",
+                           price="$1,999.95", category=category1)
 
 session.add(catalogItem2)
 session.commit()
 
-catalogItem3 = CatalogItem(name="Android Smart Phone", description="waterproof, 6 inch 4k screen, 64GB internal memory, 4GB RAM, 32 Megapixel backfacing camera, 16 MP front facing camera, fingerprint scanner, Android version 8.0",
-                           price="$499", category=category1)
+catalogItem3 = CatalogItem(user_id=1, name="Android Smart Phone", description="This is a heavily enchanted gadget that features a mix of technology and sorcery. It can project voice, images, and data between similar devices through clouds of all things.",
+                           price="$599.99", category=category1)
 
 session.add(catalogItem3)
 session.commit()
 
-catalogItem4 = CatalogItem(name="Drone", description="Quadrocopter drone with autostabilizing 32 megapixel camera, object tracking, auto hover, follow object, auto return, gps, android, 30 minutes flight time on one charge.",
-                           price="$299", category=category1)
+catalogItem4 = CatalogItem(user_id=1, name="Drone", description="Four mechanical dragon-flies carrying a cyclops eye are magically linked to an enchanted smart phone through clouds.",
+                           price="$299.50", category=category1)
 
 session.add(catalogItem4)
 session.commit()
 
 
 # Catelog Items for Category 2 (Kitchen)
-category2 = Category(name="Kitchen")
+category2 = Category(user_id=1, name="Kitchen")
 
 session.add(category2)
 session.commit()
 
 
-catalogItem1 = CatalogItem(name="Coffee Maker", description="Always ready fresh coffee, warms up in seconds, makes one cup or a whole pot.",
-                           price="89.99", category=category2)
+catalogItem1 = CatalogItem(user_id=1, name="Coffee Maker", description="Always ready fresh coffee, warms up in seconds, makes one cup or a whole pot. IV not included.",
+                           price="$89.99", category=category2)
 
 session.add(catalogItem1)
 session.commit()
 
-catalogItem2 = CatalogItem(name="Rice Cooker", description="Stainless Steel, easy to clean with removable liner, built in timer, keep warm setting",
+catalogItem2 = CatalogItem(user_id=1, name="Rice Cooker", description="Stainless Steel, easy to clean with removable liner, built in timer, keep warm setting",
                            price="$29.99", category=category2)
 
 session.add(catalogItem2)
 session.commit()
 
-catalogItem3 = CatalogItem(name="18 Piece Knife Set", description="Includes Walnut Stained wood block, 6 steak knifes, kitchen shear, knife sharpener, boning knife, peeling knife, paring knife, utility knife, serrated utility knife, slicing knife, santoku knife, bread knife, and chef knife.",
+catalogItem3 = CatalogItem(user_id=1, name="18 Piece Knife Set", description="Includes Walnut Stained wood block, 6 steak knifes, kitchen shear, knife sharpener, boning knife, peeling knife, paring knife, utility knife, serrated utility knife, slicing knife, santoku knife, bread knife, and chef knife.",
                            price="$89.98", category=category2)
 
 session.add(catalogItem3)
 session.commit()
 
-catalogItem4 = CatalogItem(name="Cast Iron Skillet", description="12.5 inches, pre-seasoned cast iron.",
+catalogItem4 = CatalogItem(user_id=1, name="Cast Iron Skillet", description="12.5 inches, pre-seasoned cast iron.",
                            price="$24.99", category=category2)
 
 session.add(catalogItem4)
 session.commit()
 
+catalogItem5 = CatalogItem(user_id=1, name="Fruit Samurai", description="This horrifying contraption brutally mutates any fruit put into it. Come to think of it, I doubt it would be any nicer to anything at all you put in it.",
+                           price="$15.95", category=category2)
+
+session.add(catalogItem5)
+session.commit()
 
 # Catelog Items for Category 3 (Automotive)
-category3 = Category(name="Automotive")
+category3 = Category(user_id=1, name="Automotive")
 
 session.add(category3)
 session.commit()
 
 
-catalogItem1 = CatalogItem(name="Jack Stand", description="One Pair of 3 Ton Jack Stands with Double Pin Safety.",
+catalogItem1 = CatalogItem(user_id=1, name="Jack Stand", description="One Pair of 3 Ton Jack Stands with Double Pin Safety.",
                            price="$29.99", category=category3)
 
 session.add(catalogItem1)
 session.commit()
 
-catalogItem2 = CatalogItem(name="Wheel Chocks", description="Prevent your vehicle from rolling with this pair of Wheel Chocks. Made from heavy duty plastic.",
+catalogItem2 = CatalogItem(user_id=1, name="Wheel Chocks", description="Prevent your vehicle from rolling with this pair of Wheel Chocks. Made from heavy duty plastic.",
                            price="$6.99", category=category3)
 
 session.add(catalogItem2)
 session.commit()
 
-catalogItem3 = CatalogItem(name="4-Way Lug Wrench", description="20 inch Crossbar provides eaxtra tork. Includes four sizes: 19mm, 7/8, 13/16, and 3/4",
+catalogItem3 = CatalogItem(user_id=1, name="4-Way Lug Wrench", description="20 inch Crossbar provides eaxtra tork. Includes four sizes: 19mm, 7/8, 13/16, and 3/4",
                            price="$20.00", category=category3)
 
 session.add(catalogItem3)
 session.commit()
 
-catalogItem4 = CatalogItem(name="5 Quarts of 5W-30 Oil", description="Fully Synthetic. 5W-30 oil. 5 quarts.",
+catalogItem4 = CatalogItem(user_id=1, name="5 Quarts of 5W-30 Oil", description="Fully Synthetic. 5W-30 oil. 5 quarts.",
                            price="$18.72", category=category3)
 
 session.add(catalogItem4)
 session.commit()
 
+catalogItem5 = CatalogItem(user_id=1, name="Headlight Fluid", description="Warning: do not use this in the wrong container. Read all labels and manufacture manuals before use. Non-refundable.",
+                           price="$9.99", category=category3)
+
+session.add(catalogItem5)
+session.commit()
+
 
 # Catelog Items for Category 4 (Garden)
-category4 = Category(name="Garden")
+category4 = Category(user_id=1, name="Garden")
 
 session.add(category4)
 session.commit()
 
 
-catalogItem1 = CatalogItem(name="Bypass Pruning Shears", description="Trim Rose Bushes to small branches with these ergonomic pruning shears.",
+catalogItem1 = CatalogItem(user_id=1, name="Pruning Shears", description="Momma told me not to play with scissors, but she didn't say anything about pruning shears! Fair warning: avoid using them on AA batteries and fingers. If you want to use them on flautas or taquitos, you might want to wash them. Do NOT wash them in the clothes washer! Momma was VERY upset after that.",
                            price="$10.21", category=category4)
 
 session.add(catalogItem1)
 session.commit()
 
-catalogItem2 = CatalogItem(name="Lawn Mower", description="Adjustable hight, gas powered, push mower with detachable mulch bag..",
+catalogItem2 = CatalogItem(user_id=1, name="Lawn Mower", description="This vicious red beast matches the capacity of 200 cows at pasture to eat grass. It prefers, and at times demands a drink more potent than moonshine - it's called gasoline. I don't much care for the stuff, but to each there own.",
                            price="$199.99", category=category4)
 
 session.add(catalogItem2)
 session.commit()
 
-catalogItem3 = CatalogItem(name="Fertilizer", description="20 pound bag of organic all season lawn food.",
+catalogItem3 = CatalogItem(user_id=1, name="Fertilizer", description="20 pound bag of organic all season lawn food. Ok, I'm not going to lie here. It's poop, dirt, and anything the pigs wouldn't eat left over from the farm. Enjoy!",
                            price="$30.40", category=category4)
 
 session.add(catalogItem3)
 session.commit()
 
-catalogItem4 = CatalogItem(name="Garden Tool Set", description="Includes carying tote, gloves, plant rope, pruner, weeder, trowel, cultivator, transplanter, and weeding fork.",
+catalogItem4 = CatalogItem(user_id=1, name="Garden Tool Set", description="Includes carying tote, gloves, plant rope, pruner, weeder, trowel, cultivator, transplanter, and weeding fork.",
                            price="$22.99", category=category4)
 
 session.add(catalogItem4)
 session.commit()
 
+catalogItem5 = CatalogItem(user_id=1, name="Smooth Rock", description="Found on the side of the road.",
+                           price="$0.99", category=category4)
 
-print "added catalog items!"
+session.add(catalogItem5)
+session.commit()
+
+
+# Catelog Items for Category 5 (Sporting Goods)
+category5 = Category(user_id=1, name="Sporting Goods")
+
+session.add(category5)
+session.commit()
+
+
+catalogItem1 = CatalogItem(user_id=1, name="Elvin Long Bow", description="50 lb pull, made from the wood of a treant to resist warping and provide extra strength and flexibility.",
+                           price="$349.99", category=category5)
+
+session.add(catalogItem1)
+session.commit()
+
+catalogItem2 = CatalogItem(user_id=1, name="Pixie Poison", description="Guaranteed to get rid of pesky pixies.",
+                           price="$39.99", category=category5)
+
+session.add(catalogItem2)
+session.commit()
+
+catalogItem3 = CatalogItem(user_id=1, name="Mithril Armor", description="Lightweight chainmail armor that is magically impenetrable. It also shimmers in moonlight.",
+                           price="$9999.00", category=category5)
+
+session.add(catalogItem3)
+session.commit()
+
+catalogItem4 = CatalogItem(user_id=1, name="Elixir of Invisibility", description="Provides 10 minutes of invisibility after drinking.",
+                           price="$49.95", category=category5)
+
+session.add(catalogItem4)
+session.commit()
+
+
+print "Added catalog items!"
