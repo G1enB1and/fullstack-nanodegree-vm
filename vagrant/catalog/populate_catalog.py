0c7718a169156494cfc2d6322b6ef7c09b8b9dc5
G1enB1and <glen.bland.81@gmail.com>
2018-10-11 18:28:58 -0500

    made populate_catalog.py pep8 compliant

--- vagrant/catalog/populate_catalog.py
+++ vagrant/catalog/populate_catalog.py
@@ -20,7 +20,9 @@ session = DBSession()
 
 
 # Create dummy user
-User1 = User(name="Totally Legit Robot", email="Friendly_Robot@Empty_Warehouse.com", picture="user_pic_1.jpg")
+User1 = User(name="Totally Legit Robot",
+             email="Friendly_Robot@Empty_Warehouse.com",
+             picture="user_pic_1.jpg")
 
 session.add(User1)
 session.commit()
@@ -32,26 +34,46 @@ category1 = Category(user_id=1, name="Electronics")
 session.add(category1)
 session.commit()
 
-
-catalogItem1 = CatalogItem(user_id=1, name="Wireless Headset", description="This wireless headset features two miniature faeries that sing, talk, and beat-box with magically enhanced accuracy inside the ear buds. Just don't ask them to play comedy. Sometimes they can't help but laugh. Have you ever heard someone laugh from inside your ear? ouch.",
+descriptionLong = '' + 'This wireless headset features two miniature '
+descriptionLong += 'faeries that sing, talk, and beat-box with magically '
+descriptionLong += 'enhanced accuracy inside the ear buds. Just don\'t '
+descriptionLong += 'ask them to play comedy. Sometimes they can\'t help '
+descriptionLong += 'but laugh. Have you ever heard someone laugh from '
+descriptionLong += 'inside your ear? ouch.'
+catalogItem1 = CatalogItem(user_id=1, name="Wireless Headset",
+                           description=descriptionLong,
                            price="$59.99", category=category1)
 
 session.add(catalogItem1)
 session.commit()
 
-catalogItem2 = CatalogItem(user_id=1, name="Big Screen TV", description="This portal into other realms is protected by a clear plastic shield that won't let you pass into it. Just don't press too hard. This particular model measures 65\".",
+descriptionLong = '' + 'This portal into other realms is protected by a '
+descriptionLong += 'clear plastic shield that won\'t let you pass into it. '
+descriptionLong += 'Just don\'t press too hard. This particular model '
+descriptionLong += 'measures 65\".'
+catalogItem2 = CatalogItem(user_id=1, name="Big Screen TV",
+                           description=descriptionLong,
                            price="$1,999.95", category=category1)
 
 session.add(catalogItem2)
 session.commit()
 
-catalogItem3 = CatalogItem(user_id=1, name="Android Smart Phone", description="This is a heavily enchanted gadget that features a mix of technology and sorcery. It can project voice, images, and data between similar devices through clouds of all things.",
+descriptionLong = '' + 'This is a heavily enchanted gadget that features a '
+descriptionLong += 'mix of technology and sorcery. It can project voice, '
+descriptionLong += 'images, and data between similar devices through clouds '
+descriptionLong += 'of all things.'
+catalogItem3 = CatalogItem(user_id=1, name="Android Smart Phone",
+                           description=descriptionLong,
                            price="$599.99", category=category1)
 
 session.add(catalogItem3)
 session.commit()
 
-catalogItem4 = CatalogItem(user_id=1, name="Drone", description="Four mechanical dragon-flies carrying a cyclops eye are magically linked to an enchanted smart phone through clouds.",
+descriptionLong = '' + 'Four mechanical dragon-flies carrying a cyclops eye '
+descriptionLong += 'are magically linked to an enchanted smart phone through '
+descriptionLong += 'the air.'
+catalogItem4 = CatalogItem(user_id=1, name="Drone",
+                           description=descriptionLong,
                            price="$299.50", category=category1)
 
 session.add(catalogItem4)
@@ -64,32 +86,48 @@ category2 = Category(user_id=1, name="Kitchen")
 session.add(category2)
 session.commit()
 
-
-catalogItem1 = CatalogItem(user_id=1, name="Coffee Maker", description="Always ready fresh coffee, warms up in seconds, makes one cup or a whole pot. IV not included.",
+descriptionLong = '' + 'Always ready fresh coffee, warms up in seconds, makes '
+descriptionLong += 'one cup or a whole pot. IV not included.'
+catalogItem1 = CatalogItem(user_id=1, name="Coffee Maker",
+                           description=descriptionLong,
                            price="$89.99", category=category2)
 
 session.add(catalogItem1)
 session.commit()
 
-catalogItem2 = CatalogItem(user_id=1, name="Rice Cooker", description="Stainless Steel, easy to clean with removable liner, built in timer, keep warm setting",
+descriptionLong = '' + 'Stainless Steel, easy to clean with removable liner, '
+descriptionLong += 'built in timer, keep warm setting.'
+catalogItem2 = CatalogItem(user_id=1, name="Rice Cooker",
+                           description=descriptionLong,
                            price="$29.99", category=category2)
 
 session.add(catalogItem2)
 session.commit()
 
-catalogItem3 = CatalogItem(user_id=1, name="18 Piece Knife Set", description="Includes Walnut Stained wood block, 6 steak knifes, kitchen shear, knife sharpener, boning knife, peeling knife, paring knife, utility knife, serrated utility knife, slicing knife, santoku knife, bread knife, and chef knife.",
+descriptionLong = '' + 'Includes Walnut Stained wood block, 6 steak knifes, '
+descriptionLong += 'kitchen shear, knife sharpener, boning knife, peeling '
+descriptionLong += 'knife, paring knife, utility knife, serrated utility '
+descriptionLong += 'knife, slicing knife, santoku knife, bread knife, and '
+descriptionLong += 'chef knife.'
+catalogItem3 = CatalogItem(user_id=1, name="18 Piece Knife Set",
+                           description=descriptionLong,
                            price="$89.98", category=category2)
 
 session.add(catalogItem3)
 session.commit()
 
-catalogItem4 = CatalogItem(user_id=1, name="Cast Iron Skillet", description="12.5 inches, pre-seasoned cast iron.",
+catalogItem4 = CatalogItem(user_id=1, name="Cast Iron Skillet",
+                           description="12.5 inches, pre-seasoned cast iron.",
                            price="$24.99", category=category2)
 
 session.add(catalogItem4)
 session.commit()
 
-catalogItem5 = CatalogItem(user_id=1, name="Fruit Samurai", description="This horrifying contraption brutally mutates any fruit put into it. Come to think of it, I doubt it would be any nicer to anything at all you put in it.",
+descriptionLong = '' + 'This horrifying contraption brutally mutates any '
+descriptionLong += 'fruit put into it. Come to think of it, I doubt it would '
+descriptionLong += 'be any nicer to anything at all you put in it.'
+catalogItem5 = CatalogItem(user_id=1, name="Fruit Samurai",
+                           description=descriptionLong,
                            price="$15.95", category=category2)
 
 session.add(catalogItem5)
@@ -101,32 +139,44 @@ category3 = Category(user_id=1, name="Automotive")
 session.add(category3)
 session.commit()
 
-
-catalogItem1 = CatalogItem(user_id=1, name="Jack Stand", description="One Pair of 3 Ton Jack Stands with Double Pin Safety.",
+descriptionLong = '' + 'One Pair of 3 Ton Jack Stands with Double Pin Safety.'
+catalogItem1 = CatalogItem(user_id=1, name="Jack Stand",
+                           description=descriptionLong,
                            price="$29.99", category=category3)
 
 session.add(catalogItem1)
 session.commit()
 
-catalogItem2 = CatalogItem(user_id=1, name="Wheel Chocks", description="Prevent your vehicle from rolling with this pair of Wheel Chocks. Made from heavy duty plastic.",
+descriptionLong = '' + 'Prevent your vehicle from rolling with this pair of '
+descriptionLong += 'Wheel Chocks. Made from heavy duty plastic.'
+catalogItem2 = CatalogItem(user_id=1, name="Wheel Chocks",
+                           description=descriptionLong,
                            price="$6.99", category=category3)
 
 session.add(catalogItem2)
 session.commit()
 
-catalogItem3 = CatalogItem(user_id=1, name="4-Way Lug Wrench", description="20 inch Crossbar provides eaxtra tork. Includes four sizes: 19mm, 7/8, 13/16, and 3/4",
+descriptionLong = '' + '20 inch Crossbar provides extra tork. Includes four '
+descriptionLong += 'sizes: 19mm, 7/8, 13/16, and 3/4.'
+catalogItem3 = CatalogItem(user_id=1, name="4-Way Lug Wrench",
+                           description=descriptionLong,
                            price="$20.00", category=category3)
 
 session.add(catalogItem3)
 session.commit()
 
-catalogItem4 = CatalogItem(user_id=1, name="5 Quarts of 5W-30 Oil", description="Fully Synthetic. 5W-30 oil. 5 quarts.",
+catalogItem4 = CatalogItem(user_id=1, name="5 Quarts of 5W-30 Oil",
+                           description="Fully Synthetic. 5W-30 oil. 5 quarts.",
                            price="$18.72", category=category3)
 
 session.add(catalogItem4)
 session.commit()
 
-catalogItem5 = CatalogItem(user_id=1, name="Headlight Fluid", description="Warning: do not use this in the wrong container. Read all labels and manufacture manuals before use. Non-refundable.",
+descriptionLong = '' + 'Warning: do not use this in the wrong container. '
+descriptionLong += 'Read all labels and manufacture manuals before use. '
+descriptionLong += 'Non-refundable.'
+catalogItem5 = CatalogItem(user_id=1, name="Headlight Fluid",
+                           description=descriptionLong,
                            price="$9.99", category=category3)
 
 session.add(catalogItem5)
@@ -139,32 +189,54 @@ category4 = Category(user_id=1, name="Garden")
 session.add(category4)
 session.commit()
 
-
-catalogItem1 = CatalogItem(user_id=1, name="Pruning Shears", description="Momma told me not to play with scissors, but she didn't say anything about pruning shears! Fair warning: avoid using them on AA batteries and fingers. If you want to use them on flautas or taquitos, you might want to wash them. Do NOT wash them in the clothes washer! Momma was VERY upset after that.",
+descriptionLong = '' + 'Momma told me not to play with scissors, but she '
+descriptionLong += 'didn\'t say anything about pruning shears! Fair warning: '
+descriptionLong += 'avoid using them on AA batteries and fingers. If you want '
+descriptionLong += 'to use them on flautas or taquitos, you might want to '
+descriptionLong += 'wash them. Do NOT wash them in the clothes washer! Momma '
+descriptionLong += 'was VERY upset after that.'
+catalogItem1 = CatalogItem(user_id=1, name="Pruning Shears",
+                           description=descriptionLong,
                            price="$10.21", category=category4)
 
 session.add(catalogItem1)
 session.commit()
 
-catalogItem2 = CatalogItem(user_id=1, name="Lawn Mower", description="This vicious red beast matches the capacity of 200 cows at pasture to eat grass. It prefers, and at times demands a drink more potent than moonshine - it's called gasoline. I don't much care for the stuff, but to each there own.",
+descriptionLong = '' + 'This vicious red beast matches the capacity of 200 '
+descriptionLong += 'cows at pasture to eat grass. It prefers, and at times '
+descriptionLong += 'demands a drink more potent than moonshine - it\'s called '
+descriptionLong += 'gasoline. I don\'t much care for the stuff, but to each '
+descriptionLong += 'there own.'
+catalogItem2 = CatalogItem(user_id=1, name="Lawn Mower",
+                           description=descriptionLong,
                            price="$199.99", category=category4)
 
 session.add(catalogItem2)
 session.commit()
 
-catalogItem3 = CatalogItem(user_id=1, name="Fertilizer", description="20 pound bag of organic all season lawn food. Ok, I'm not going to lie here. It's poop, dirt, and anything the pigs wouldn't eat left over from the farm. Enjoy!",
+descriptionLong = '' + '20 pound bag of organic all season lawn food. Ok, '
+descriptionLong += 'I\'m not going to lie here. It\'s poop, dirt, and '
+descriptionLong += 'anything the pigs wouldn\'t eat left over from the farm. '
+descriptionLong += 'Enjoy!'
+catalogItem3 = CatalogItem(user_id=1, name="Fertilizer",
+                           description=descriptionLong,
                            price="$30.40", category=category4)
 
 session.add(catalogItem3)
 session.commit()
 
-catalogItem4 = CatalogItem(user_id=1, name="Garden Tool Set", description="Includes carying tote, gloves, plant rope, pruner, weeder, trowel, cultivator, transplanter, and weeding fork.",
+descriptionLong = '' + 'Includes carying tote, gloves, plant rope, pruner, '
+descriptionLong += 'weeder, trowel, cultivator, transplanter, and weeding '
+descriptionLong += 'fork.'
+catalogItem4 = CatalogItem(user_id=1, name="Garden Tool Set",
+                           description=descriptionLong,
                            price="$22.99", category=category4)
 
 session.add(catalogItem4)
 session.commit()
 
-catalogItem5 = CatalogItem(user_id=1, name="Smooth Rock", description="Found on the side of the road.",
+catalogItem5 = CatalogItem(user_id=1, name="Smooth Rock",
+                           description="Found on the side of the road.",
                            price="$0.99", category=category4)
 
 session.add(catalogItem5)
@@ -177,26 +249,35 @@ category5 = Category(user_id=1, name="Sporting Goods")
 session.add(category5)
 session.commit()
 
-
-catalogItem1 = CatalogItem(user_id=1, name="Elvin Long Bow", description="50 lb pull, made from the wood of a treant to resist warping and provide extra strength and flexibility.",
+descriptionLong = '' + '50 lb pull, made from the wood of a treant to resist '
+descriptionLong += 'warping and provide extra strength and flexibility.'
+catalogItem1 = CatalogItem(user_id=1, name="Elvin Long Bow",
+                           description=descriptionLong,
                            price="$349.99", category=category5)
 
 session.add(catalogItem1)
 session.commit()
 
-catalogItem2 = CatalogItem(user_id=1, name="Pixie Poison", description="Guaranteed to get rid of pesky pixies.",
+descriptionLong = '' + 'Guaranteed to get rid of pesky pixies.'
+catalogItem2 = CatalogItem(user_id=1, name="Pixie Poison",
+                           description=descriptionLong,
                            price="$39.99", category=category5)
 
 session.add(catalogItem2)
 session.commit()
 
-catalogItem3 = CatalogItem(user_id=1, name="Mithril Armor", description="Lightweight chainmail armor that is magically impenetrable. It also shimmers in moonlight.",
+descriptionLong = '' + 'Lightweight chainmail armor that is magically '
+descriptionLong += 'impenetrable. It also shimmers in moonlight.'
+catalogItem3 = CatalogItem(user_id=1, name="Mithril Armor",
+                           description=descriptionLong,
                            price="$9999.00", category=category5)
 
 session.add(catalogItem3)
 session.commit()
 
-catalogItem4 = CatalogItem(user_id=1, name="Elixir of Invisibility", description="Provides 10 minutes of invisibility after drinking.",
+descriptionLong = '' + 'Provides 10 minutes of invisibility after drinking.'
+catalogItem4 = CatalogItem(user_id=1, name="Elixir of Invisibility",
+                           description=descriptionLong,
                            price="$49.95", category=category5)
 
 session.add(catalogItem4)
