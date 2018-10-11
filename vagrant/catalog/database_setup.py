ebc9be347747b5a67c019077bf88cd500be1ed0d
G1enB1and <glen.bland.81@gmail.com>
2018-10-11 18:33:07 -0500

    made database_setup.py pep8 compliant

--- vagrant/catalog/database_setup.py
+++ vagrant/catalog/database_setup.py
@@ -18,42 +18,45 @@ class User(Base):
 
 
 class Category(Base):
-	__tablename__ = 'category'
-	
-	id = Column(Integer, primary_key=True)
-	name = Column(String(250), nullable=False)
-	user_id = Column(Integer, ForeignKey('user.id'))
-	user = relationship(User)
-	
-	@property
-	def serialize(self):
-		"""Return object data in easily serializeable format"""
-		return {'name': self.name,'id': self.id,}
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id,
+            }
 
 
 class CatalogItem(Base):
-	__tablename__ = 'catalog_item'
-	
-	name = Column(String(80), nullable=False)
-	id = Column(Integer, primary_key=True)
-	description = Column(String(250))
-	price = Column(String(8))
-	category_id = Column(Integer, ForeignKey('category.id'))
-	category = relationship(Category)
-	user_id = Column(Integer, ForeignKey('user.id'))
-	user = relationship(User)
-	
-	@property
-	def serialize(self):
-		"""Return object data in easily serializeable format"""
-		return {
-			'name': self.name,
-			'description': self.description,
-			'id': self.id,
-			'price': self.price,
-			'category_id': self.category_id,
-			'category_name': self.category.name,
-			}
+    __tablename__ = 'catalog_item'
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    description = Column(String(250))
+    price = Column(String(8))
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+            'price': self.price,
+            'category_id': self.category_id,
+            'category_name': self.category.name,
+            }
 
 
 engine = create_engine('sqlite:///catalog.db')
